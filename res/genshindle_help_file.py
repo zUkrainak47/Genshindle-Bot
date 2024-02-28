a050cab87602e1b759bf712b50c6f55022fcc597
zUkrainak47 <55208577+zUkrainak47@users.noreply.github.com>
2024-02-28 11:50:58 +0200

    added extra logs

--- res/genshindle_help_file.py
+++ res/genshindle_help_file.py
@@ -224,6 +224,7 @@ def identify_region(character, t, el_reg, even_faster):
         print("Too fast to identify region, waiting 0.1 second to try again")
         sleep(0.1)
         t = screenshot(location)
+        t.save(r'.\logs\region.png')
         r, g, b = t.getpixel((615, 15))
     print(f"Region identification failed: ({r}, {g}, {b})")
 
@@ -242,6 +243,7 @@ def identify_vision(character, t, el_vis, even_faster):
         print("Too fast to identify vision, waiting 0.1 second to try again")
         sleep(0.1)
         t = screenshot(location)
+        t.save(r'.\logs\vision.png')
         r, g, b = t.getpixel((725, 15))
     print(f"Vision identification failed: ({r}, {g}, {b})")
 
@@ -260,6 +262,7 @@ def identify_weapon(character, t, el_weap, even_faster):
         print("Too fast to identify weapon, waiting 0.1 second to try again")
         sleep(0.1)
         t = screenshot(location)
+        t.save(r'.\logs\weapon.png')
         r, g, b = t.getpixel((835, 15))
     print(f"Weapon identification failed: ({r}, {g}, {b})")
 
